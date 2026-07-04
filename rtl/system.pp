8d076541976d83737f8e0eb681ab90b9fe1fc05e
Danny Milosavljevic <dannym@friendly-machines.com>
2026-07-04 21:57:08 +0200

    rtl: Add external decls

--- rtl/system.pp
+++ rtl/system.pp
@@ -3,7 +3,16 @@ unit system;
 interface
 
 type 
+  Byte = external nil name 't_byte';
+  ShortInt = external nil name 't_shortint';
+  Word = external nil name 't_word';
+  SmallInt = external nil name 't_smallint';
+  Cardinal = external nil name 't_cardinal';
+  Integer = external nil name 't_integer';
+  LongInt = external nil name 't_longint';
   Boolean = (False, True);
+  Char = external nil name 't_char';
+  QWord = external nil name 't_qword';
   
 implementation
 
