b58b61c7a1e6e15ed25e4f1cc55335916424d64b
Danny Milosavljevic <dannym@friendly-machines.com>
2026-07-09 00:13:07 +0200

    emit: Fix superclass refs

--- src/emit.cc
+++ src/emit.cc
@@ -454,29 +454,29 @@ void Emitter::emit_aggregate_decl(std::string cxx_name, Type* ty, bool in_meta) 
 			if (in_meta) {
 				super_cxx_name = super_cxx_name + "::m_meta";
 			}
-			fprintf(active, "public %s", super_cxx_name.c_str());
-			first = false;
-		}
-		if (!in_meta) {
-			for (auto interface_type : c->implemented_interfaces) {
-				fprintf(active, first ? "public %s" : ", public %s",
-					interface_type->cxx_name.c_str());
+				fprintf(active, " : public %s", super_cxx_name.c_str());
 				first = false;
 			}
-		} else {
+			if (!in_meta) {
+				for (auto interface_type : c->implemented_interfaces) {
+					fprintf(active, first ? " : public %s" : ", public %s",
+						interface_type->cxx_name.c_str());
+					first = false;
+				}
+			} else {
 			// not sure. FIXME: m_iobject ?
 		}
 	} else if (auto c = dynamic_cast<InterfaceType*>(ty)) {
-		bool first = true;
-		for (auto interface_type : c->super_interfaces) {
-			fprintf(active, first ? "public %s" : ", public %s",
-				interface_type->cxx_name.c_str());
-			first = false;
+			bool first = true;
+			for (auto interface_type : c->super_interfaces) {
+				fprintf(active, first ? " : public %s" : ", public %s",
+					interface_type->cxx_name.c_str());
+				first = false;
+			}
+		} else if (auto c = dynamic_cast<ObjectType*>(ty)) {
+			if (c->super)
+				fprintf(active, " : public %s", c->super->cxx_name.c_str());
 		}
-	} else if (auto c = dynamic_cast<ObjectType*>(ty)) {
-		if (c->super)
-			fprintf(active, "public %s", c->super->cxx_name.c_str());
-	}
 
 	fprintf(active, " {\n");
 	if (is_class && in_meta) {
