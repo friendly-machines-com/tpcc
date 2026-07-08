c33c60e07f28e212dcd32cebbc44fdb4431fff21
Danny Milosavljevic <dannym@friendly-machines.com>
2026-07-08 23:24:24 +0200

    emit: Fix metaclass emission

--- src/emit.cc
+++ src/emit.cc
@@ -1002,10 +1002,6 @@ void Emitter::emit_type_ref(Type* ty) {
 		return;
 	}
 	if (auto r = dynamic_cast<ClassRefType*>(ty)) {
-		if (!r->cxx_name.empty()) {
-			// FIXME
-			return;
-		}
 		ty = r->target;
 		// Follow IncompleteType placeholders through to the real underlying type.
 		while (auto inc = dynamic_cast<IncompleteType*>(ty)) {
@@ -1018,11 +1014,11 @@ void Emitter::emit_type_ref(Type* ty) {
 				emit_aggregate_decl("", ty);
 			else
 				fprintf(active, "%s", c->cxx_name.c_str());
-			fprintf(active, "::m_meta");
-			fprintf(active, "*");
 		} else {
 			unhandled_type("emit_type_ref", ty);
 		}
+		fprintf(active, "::m_meta");
+		fprintf(active, "*");
 		return;
 	}
 	if (auto c = dynamic_cast<InterfaceType*>(ty)) {
