763c2e2c49367dc1f1997ce6b14f364d02f63713
Danny Milosavljevic <dannym@friendly-machines.com>
2026-07-12 08:10:43 +0200

    builtins: Add OctStr

--- src/builtins.cc
+++ src/builtins.cc
@@ -385,6 +385,7 @@ static const BuiltinDesc k_builtins[] = {
     {"pas::p_dec", nullptr, {}, BuiltinGenericKind::OrdinalMutation},
     {"pas::p_str", nullptr},
     {"pas::p_val", nullptr},
+    {"pas::p_octstr", nullptr},
     {"pas::p_low", nullptr, TypeBoundKind::Low},
     {"pas::p_high", nullptr, TypeBoundKind::High},
     {"pas::p_setlength", nullptr},
