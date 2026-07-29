d73bc558f25c123a17a4193798bb50f13bdb0617
Danny Milosavljevic <dannym@friendly-machines.com>
2026-07-29 21:30:55 +0200

    tests: Add operator_catalog.cc

---
+++ tests/operator_catalog.cc
@@ -0,0 +1,25 @@
+#include "../src/operators.h"
+
+#include <cassert>
+
+int main() {
+	auto explicit_conversion =
+	    explicit_operator_identifier();
+	assert(explicit_conversion ==
+	       "&op_Explicit");
+
+	auto bitwise_xor = operator_invocation_identifier(
+	    OperatorInvocation::BinaryToken,
+	    "xor", 2, false, false);
+	assert(bitwise_xor);
+	assert(*bitwise_xor == "&op_BitwiseXor");
+
+	bool saw_initialize = false;
+	for (const OperatorSpec& spec : operator_catalog()) {
+		if (spec.declaration_name != "initialize")
+			continue;
+		saw_initialize = true;
+		assert(!spec.declaration_supported);
+	}
+	assert(saw_initialize);
+}
